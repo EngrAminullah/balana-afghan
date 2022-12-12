37ac9d5b0d0e8975ec637cc12c51d3514c00473b
Engr Aminullah <96494231+EngrAminullah@users.noreply.github.com>
2022-12-12 12:35:13 +0500

    hero slider

--- src/assets/fake-data/slider.js
+++ src/assets/fake-data/slider.js
@@ -1,11 +1,11 @@
 import sliderImg01 from "../images/slider1.png";
 import sliderImg02 from "../images/slider2.png";
-import sliderImg03 from "../images/slider03";
+import sliderImg03 from "../images/slider03.png";
 
-export default sliderData = [
+export const sliderData = [
   {
     id: "01",
-    title: "We have simple and delicious food for you",
+    title: "We have simple and delicious food for you 😍",
     desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi animi, itaque ipsum magnam quae alias omnis veniam dolor fugiat similique voluptas consectetur corrupti odit quisquam incidunt quia architecto voluptate molestiae?",
     imgUrl: sliderImg01,
   },
--- src/components/hero-slider/HeroSlider.jsx
+++ src/components/hero-slider/HeroSlider.jsx
@@ -1,22 +1,26 @@
 import React from "react";
 import { Container } from "reactstrap";
 import Slider from "react-slick";
-import sliderData from "../../assets/fake-data/slider";
+import { sliderData } from "../../assets/fake-data/slider";
 import "./slider.css";
 
 const HeroSlider = () => {
   return (
     <section>
       <Container>
-        <Slider>
+        <Slider className="slider-btn">
           {sliderData.map((item) => (
             <div>
-              <div className="slider_wrapper">
-                <div className="slider_content">
-                  <h2>{item.title}</h2>
+              <div
+                className="slider_wrapper d-flex align-items-center 
+              justify-content-between pt-5"
+              >
+                <div className="slider_content w-50 ps-2">
+                  <h2 className="mb-3">{item.title}</h2>
                   <p>{item.desc}</p>
+                  <button className="btn">Explore Food</button>
                 </div>
-                <div className="slider_img">
+                <div className="slider_img w-50">
                   <img src={item.imgUrl} alt="" className="w-100" />
                 </div>
               </div>
