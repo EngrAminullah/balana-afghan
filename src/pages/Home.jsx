50b1b93e3dfb698e029e2991b0d3dfbb763ed480
Engr Aminullah <96494231+EngrAminullah@users.noreply.github.com>
2022-12-19 14:57:22 +0500

    kj

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,22 +7,22 @@ import HeroSlider from "../components/hero-slider/HeroSlider";
 import MenuPack from "../components/menu-pack/MenuPack";
 import PopularMenu from "../components/popular-menu/PopularMenu";
 import Testimonials from "../components/testimonials/Testimonials";
-import { useNav } from '../customHooks/useNav';
+import { useNav } from "../customHooks/useNav";
 
 const Home = () => {
-  const HeroRef = useNav('Hero');
-  const MenuRef = useNav('Menu');
-  const AboutRef = useNav('About');
-  const ContacttRef = useNav(' Contact')
+  const HeroRef = useNav("Hero");
+  const MenuRef = useNav("Menu");
+  const AboutRef = useNav("About");
+  const ContacttRef = useNav(" Contact");
   return (
     <Fragment>
       <Header />
       <section id="heroContainer" ref={HeroRef}>
         <HeroSlider />
       </section>
-      <section   id="menuContainer" ref={MenuRef}>
-        <MenuPack />
+      <section id="menuContainer" ref={MenuRef}>
         <PopularMenu />
+        <MenuPack />
       </section>
       <section id="aboutContainer" ref={AboutRef}>
         <ChooseUs />
